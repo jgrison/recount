9cd7c58562c08a2f81622457dfcac9fa0514a098
Jeff <jgrison@vicimus.com>
2014-09-30 21:21:40 -0400

    Outlining routes and views

--- recount/__init__.py
+++ recount/__init__.py
@@ -1,5 +1,36 @@
-from flask import Flask
+from flask import Flask, request, g, redirect, render_template, url_for, flash, request
 app = Flask(__name__)
+
+# Homepage
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+# Lists all the reports we've made
+@app.route('/reports')
+def reports():
+	return render_template('reports/index.html')
+
+# Generate New Report
+@app.route('/reports/generate', methods=['POST'])
+def generate_report():
+	return redirect(url_for('reports'))
+
+# Create a new report type
+@app.route('/reports/build/add')
+def add_report():
+	return render_template('reports/add.html')
+
+# Edit a report type
+@app.route('/reports/build/edit/<id>')
+def edit_report(id):
+	return render_template('reports/add.html')
+
+# Delete a report type
+@app.route('/reports/build/delete/<id>')
+def delete_report(id):
+	return redirect(url_for('reports'))
+	
 
 if __name__ == "__main__":
     app.run()
