aac5ec9cb0f5626830a90fba5d165a9b73594cd9
Jeff <jgrison@vicimus.com>
2014-09-30 21:24:23 -0400

    view to remove past reports

--- recount/__init__.py
+++ recount/__init__.py
@@ -16,19 +16,24 @@ def reports():
 def generate_report():
 	return redirect(url_for('reports'))
 
+# Delete a past report
+@app.route('/reports/delete/<id>')
+def delete_report():
+	return render_template('reports/index.html')
+
 # Create a new report type
 @app.route('/reports/build/add')
-def add_report():
+def add_report_type():
 	return render_template('reports/add.html')
 
 # Edit a report type
 @app.route('/reports/build/edit/<id>')
-def edit_report(id):
+def edit_report_type(id):
 	return render_template('reports/add.html')
 
 # Delete a report type
 @app.route('/reports/build/delete/<id>')
-def delete_report(id):
+def delete_report_type(id):
 	return redirect(url_for('reports'))
 	
 
